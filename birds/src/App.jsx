44773b99decefaf074b1ebeaa3cb75ac5aad1d32
sophie-from <ponomarevasophie@gmail.com>
2025-03-07 01:19:06 +0100

    7th commit Birds

--- birds/src/App.jsx
+++ birds/src/App.jsx
@@ -2,21 +2,21 @@ import BirdFinder from "./components/BirdFinder";
 import Navigation from './components/nav';
 import Header from './components/header';
 import Footer from './components/footer';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 import Quiz from "./pages/quiz";
 import HomePage from "./pages/HomePage.jsx";
 
 function App() {
 
   return (
-<BrowserRouter>
+<HashRouter>
 <Header/>
 <Routes>
   <Route path="/" element={<HomePage/>}></Route>
   <Route path="quiz" element={<Quiz/>}></Route>
   </Routes>
     <Footer/>    
-    </BrowserRouter> )
+    </HashRouter> )
 }
 
 export default App;
